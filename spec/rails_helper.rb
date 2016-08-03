0182523efd7617d5138baf0d6aeee5543a6dc570
wuhlcom <378433855@qq.com>
2016-08-03 13:21:26 -0400

    rspec test title

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -48,5 +48,6 @@ RSpec.configure do |config|
   #
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
+  config.include Capybara::DSL
   config.infer_spec_type_from_file_location!
 end
