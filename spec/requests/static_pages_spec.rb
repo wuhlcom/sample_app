45cf43a724fb528ab24a43bb1e2f86701979b3d9
wuhlcom <wuhlcom@localhost.localdomain>
2016-08-09 03:18:39 -0700

    import bootstrap style

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -20,7 +20,24 @@ RSpec.describe "StaticPages", type: :request do
       visit static_pages_home_path
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
    end 
- end
+ 
+   #help method test   
+   it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+
+    it "should have the base title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
+  end
+
 
   describe "Help page" do
     it "should have the content 'Help'" do
