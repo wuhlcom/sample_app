6803e30870246b047b2d192d66fe23359dbf6ec2
wuhlcom <378433855@qq.com>
2016-08-13 10:42:03 -0400

    signup page

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "UserPages", type: :request do
+ subject {page} 
+ describe "GET /user_pages" do
+    it "works! (now write some real specs)" do
+      get user_pages_index_path
+      expect(response).to have_http_status(200)
+    end
+  end
+ describe "signup page" do
+  before { visit signup_path }
+  it {should have_content('Sign up')}
+  it {should have_title(full_title('Sign up'))}
+ end
+end
