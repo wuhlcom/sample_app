3db732ab06a54784eb09f1f595964bfb520e6c55
wuhlcom <378433855@qq.com>
2016-08-13 12:14:03 -0400

    rebuild rspec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,29 +10,52 @@ RSpec.describe "StaticPages", type: :request do
  # end
   
  subject {page} 
+ shared_examples_for "all static pages" do
+   it { should have_content(heading) }
+   it { should have_title(full_title(page_title))}
+ end 
+ 
  describe "Home page" do
-   before {visit root_path}
-   it {should have_content('Sample App')}
-   it {should have_title(full_title(""))}
-   it {should_not have_title('| Home')}
+  before {visit root_path}
+  let(:heading){'Sample App'}
+  let(:page_title){""}
+  it_should_behave_like "all static pages"
+  it {should_not have_title('| Home')}
  end
 
  describe "Help page" do
-   before {visit help_path}
-   it {should have_content('Help')}
-   it {should have_title(full_title("Help"))}
+  before {visit help_path}
+  let(:heading){'Help'}
+  let(:page_title){"Help"}
+  it_should_behave_like "all static pages"
  end
  
  describe "About page" do
-   before {visit about_path}
-   it {should have_content('About Us')}
-   it {should have_title(full_title("About"))}
+  before {visit about_path}
+  let(:heading){'About Us'}
+  let(:page_title){"About"}
+  it_should_behave_like "all static pages"
  end
 
  describe "Contact page" do
-   before {visit contact_path}  
-   it {should have_content('Contact')}
-   it {should have_title(full_title("Contact"))}
+  before {visit contact_path}  
+  let(:heading){'Contact Us'}
+  let(:page_title){"Contact"}
+  it {should have_selector("h1",text:'Contact')}
+  it_should_behave_like "all static pages"
  end
-
+ it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+#    click_link "Help"
+ #   expect(page).to # fill in
+  #  click_link "Contact"
+   # expect(page).to # fill in
+    #click_link "Home"
+    #click_link "Sign up now!"
+    #expect(page).to # fill in
+    #click_link "sample app"
+    #expect(page).to # fill in
+  end
 end
