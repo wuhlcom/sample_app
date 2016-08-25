02a7ae6463d5b9b9a2113f45e7db884a0c898bbf
wuhlcom <378433855@qq.com>
2016-08-25 11:28:10 -0400

    edit view

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,54 +4,70 @@ RSpec.describe "Users", type: :request do
   subject {page} 
 
   describe "signup page" do
-    before { visit signup_path }
-    it {should have_http_status(200)}
-    it {should have_content('Sign up')}
-    it {should have_title(full_title('Sign up'))}
+        before { visit signup_path }
+    	it {should have_http_status(200)}
+    	it {should have_content('Sign up')}
+    	it {should have_title(full_title('Sign up'))}
   end
   
   describe "profile page" do
-   # Replace with code to make a user variable
-    let(:user) {FactoryGirl.create(:user)}
-    before { visit user_path(user) }
+   	# Replace with code to make a user variable
+    	let(:user) {FactoryGirl.create(:user)}
+   	before { visit user_path(user) }
 
-    it {should have_content(user.name)}
-    it {should have_title(full_title(user.name))}
+    	it {should have_content(user.name)}
+        it {should have_title(full_title(user.name))}
   end
 
 
-  describe "signup" do
+ describe "signup" do
 
-    before { visit signup_path }
+   	 before { visit signup_path }
 
-    let(:submit) { "Create my account" }
+   	 let(:submit) { "Create my account" }
 
-    describe "with invalid information" do
-      it "should not create a user" do
-        expect { click_button submit }.not_to change(User, :count)
-      end
-    end
+    	 describe "with invalid information" do
+      		 it "should not create a user" do
+         		expect { click_button submit }.not_to change(User, :count)
+     		 end
+ 	 end
 
-    describe "with valid information" do
-      before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
-      end
+	 describe "with valid information" do
+     	  	before do
+        		fill_in "Name",         with: "Example User"
+	        	fill_in "Email",        with: "user@example.com"
+       			fill_in "Password",     with: "foobar"
+        		fill_in "Confirmation", with: "foobar"
+      	  	end
 
-      it "should create a user" do
-        expect { click_button submit }.to change(User, :count).by(1)
-      end
+     		 it "should create a user" do
+       			 expect { click_button submit }.to change(User, :count).by(1)
+     		 end
     
-      describe "after saving the user" do
-        before { click_button submit }
-        let(:user) { User.find_by(email: 'user@example.com') }
+      		describe "after saving the user" do
+       			 before { click_button submit }
+       			 let(:user) { User.find_by(email: 'user@example.com') }
 
-        it { should have_title(user.name) }
-        it { should have_link("Sign out") }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-     end
-    end
+        		it { should have_title(user.name) }
+        		it { should have_link("Sign out") }
+        		it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+    		 end
+ end
+
+  describe "edit" do
+	let(:user){ FactoryGirl.create(:user) }
+   	
+    	before { visit edit_user_path(user) }
+   
+  	describe "page" do
+     		it { should have_content("Update your profile") }
+     		it { should have_title("Edit user") }
+     		it { should have_link('change',href:'http://gravatar.com/emails') }
+   	end
+   
+   	describe "with invalid information"
+      		before { click_button "Save changes"}
+      		it { should have_content('error') }
+   	end
   end
 end
