12ef27d390271ea6e65fafed0675caea7f700089
wuhlcom <wuhlcom@localhost.localdomain>
2016-08-26 03:18:20 -0700

    update usr info 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,23 @@ class UsersController < ApplicationController
       render 'new'
     end
   end 
+  
   def edit
     @user = User.find(params[:id]) 
   end
+  
+  def update
+      @user=User.find(params[:id])
+	if @user.update_attributes(user_params)
+		# Handle a successful update.
+		sign_in @user
+		flash[:success]="Update user info sucessfully"
+		redirect_to @user
+	else
+		render 'edit'
+	end
+  end
+
   private
 
   def user_params
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -32,14 +32,16 @@ RSpec.describe "AuthenticationPages", type: :request do
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
-      end
+      #before do
+       # fill_in "Email",    with: user.email.upcase
+        #fill_in "Password", with: user.password
+        #click_button "Sign in"
+      #end
+      before { sign_in user}
 
       it { should have_title(user.name) }
       it { should have_link('Profile',     href: user_path(user)) }
+      it { should have_link('Settings',    href: edit_user_path(user) }
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
       
@@ -47,6 +49,27 @@ RSpec.describe "AuthenticationPages", type: :request do
         before { click_link "Sign out" }
         it { should have_link('Sign in') }
       end
-    end
+    end #end of with valid information
+  end #end of signin page
+  
+ describe "authorization" do
+
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { patch user_path(user) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+      end #end of in the users controller
+    end #end fo for non-signed-in users
   end
 end
