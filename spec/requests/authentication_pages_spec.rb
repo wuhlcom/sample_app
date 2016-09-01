61397a1817cd493741278fbb94f57982c43114b9
wuhlcom <wuhlcom@localhost.localdomain>
2016-08-31 21:05:21 -0700

    complete updating-users

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -99,6 +99,17 @@ RSpec.describe "AuthenticationPages", type: :request do
       end
 
     end #end of as wrong user
-
+	
+    describe "as non-admin user" do
+	let(:user){FactoryGirl.create(:user)}
+	let(:non_admin){FactoryGirl.create(:user)}
+	
+	before { sign_in non_admin,no_capybara:true }
+	
+	describe "submitting a DELETE request to the Users#destroy action" do
+		before { delete user_path(user) }
+		specify { expect(response).to redirect_to(root_path) }
+	end
+   end #end of as non-admin user
   end #end of authorization
 end
