08ce64f3821ba68f8bad1f0c14b71ead709d6784
wuhlcom <378433855@qq.com>
2016-09-06 12:57:32 -0400

    following

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ActiveRecord::Base
   has_many :microposts,dependent: :destroy
   has_many :relationships,foreign_key: "follower_id",dependent: :destroy
   has_many :followed_users,through: :relationships, source: :followed
+  has_many :reverse_relationships,foreign_key: "followed_it",
+				  class_name: "Relationship",
+				  dependent:	:destroy
+  has_many :followers,through: :reverse_relationships,source: :follower
+
 
   before_save {self.email=email.downcase} 
   before_create :create_remember_token 
@@ -26,6 +31,18 @@ class User < ActiveRecord::Base
   def feed
 	# This is prelimiary. See "Following users" for the full implementation.
 	Micropost.where("user_id = ?",id)
+  end
+
+  def following?(other_user)
+	relationships.find_by(followed_id:other_user.id)
+  end
+
+  def follow!(other_user)
+	relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+	relationships.find_by(followed_id: other_user.id).destroy
   end
 
   private
