74f71334eb02d40d39218543fc8c4a8128274a0d
wuhlcom <wuhlcom@localhost.localdomain>
2016-09-06 02:45:22 -0700

    finished chapter 10

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -28,7 +28,7 @@ class MicropostsController < ApplicationController
 		end
 
 		def correct_user
-			@micropost = current_user.microposts.find_by(id: params[d])
+			@micropost = current_user.microposts.find_by(id: params[:id])
 			redirect_to root_url if @micropost.nil?
 		end	
 end
