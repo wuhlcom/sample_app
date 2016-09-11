73989e3dd8ff089b8002a0e334ee2f52623f6cf9
wuhlcom <378433855@qq.com>
2016-09-11 10:57:00 -0400

    modify micropost

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -7,7 +7,6 @@ class Micropost < ActiveRecord::Base
 	def self.from_users_followed_by(user)
 		#followed_user_ids=user.followed_user_ids
 		followed_user_ids="SELECT followed_id FROM relationships WHERE follower_id=:user_id"
-		where("user_id IN (:followed_user_ids) OR user_id=:user_id",
-			followed_user_ids: followed_user_ids,user_id: user)
+		where("user_id IN (#{followed_user_ids}) OR user_id=:user_id",user_id: user.id)
 	end
 end
